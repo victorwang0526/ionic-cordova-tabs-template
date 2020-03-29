f886cb1b59da2c7cf05480357f3409e699a4eaec
victor <fengxuanwang@qq.com>
2020-03-29 20:39:23 +0800

    login page

---
+++ src/pages/login/login.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { AlertController, LoadingController, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import {BarcodeScanner} from "@ionic-native/barcode-scanner";
+import {UserProvider} from "../../providers/user/user";
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  username: string = '';
+  password: string = '';
+
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController,
+              public userService: UserProvider,
+              public loadingController: LoadingController,
+              private storage: Storage,
+              private barcodeScanner: BarcodeScanner) {
+  }
+
+  async login() {
+    if(!this.username || !this.password) {
+      this.alertCtrl.create({title: '请输入用户名密码'}).present({});
+      return;
+    }
+    const loading = await this.loadingController.create({});
+    await loading.present();
+    this.userService.login(this.username, this.password)
+      .subscribe((res) => {
+        if(!res.success) {
+          this.alertCtrl.create({title: res.errorMessage}).present({});
+          return;
+        }
+      }, error => {
+        this.alertCtrl.create({message: JSON.stringify(error)}).present({});
+      }, () => {
+        loading.dismiss();
+      })
+  }
+
+  openQr() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      let uid = barcodeData.text;
+
+      if(!uid) {
+        this.alertCtrl.create({message: '二维码不正确，请重新扫描'}).present({});
+        return;
+      }
+
+    }).catch(err => {
+      this.alertCtrl.create({title: JSON.stringify(err)}).present({});
+    });
+  }
+}
